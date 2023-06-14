716b6426b3f846bd16e83eba5114e9f7a5b2eab7
poacher286 <bulandsahu50@gmail.com>
2023-06-15 02:34:35 +0530

    gpt4all uses added

--- gpt4all_direct.py
+++ gpt4all_direct.py
@@ -1,6 +1,6 @@
 import gpt4all
 gptj = gpt4all.GPT4All(
     model_name='ggml-gpt4all-j-v1.3-groovy',
-    model_path="models")
+    model_path="models") # Download The Model Place In The Given Folder.
 messages = [{"role": "user", "content": "Name 3 colors"}]
 gptj.chat_completion(messages)
