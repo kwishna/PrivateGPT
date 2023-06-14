b2339084796711223de371fd110e4e51011708f7
kwishna <kwishna@gmail.com>
2023-06-15 02:39:10 +0530

    gpt4all uses added

--- gpt4all_direct.py
+++ gpt4all_direct.py
@@ -1,6 +1,16 @@
 import gpt4all
+from gpt4all import GPT4All
+
 gptj = gpt4all.GPT4All(
     model_name='ggml-gpt4all-j-v1.3-groovy',
-    model_path="models") # Download The Model Place In The Given Folder.
+    model_path="models")
+
+# Download The Model Place In The Given Folder.
+# If the path not given, it will be downloaded to default directory ~/.cache/gpt4all/..
+# It may consume a couple of GB of space.
+# model list https://gpt4all.io/models/models.json
+# GPT4All.download_model(model_filename=, model_path=)
+# GPT4All.list_models();
+
 messages = [{"role": "user", "content": "Name 3 colors"}]
 gptj.chat_completion(messages)
