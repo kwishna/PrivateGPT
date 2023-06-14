c872427038e8c95510d437638d1228aeb4c39cc3
poacher286 <bulandsahu50@gmail.com>
2023-06-15 01:24:40 +0530

    selenium python documentation

--- privateGPT.py
+++ privateGPT.py
@@ -17,6 +17,7 @@ model_type = os.environ.get('MODEL_TYPE')
 model_path = os.environ.get('MODEL_PATH')
 model_n_ctx = os.environ.get('MODEL_N_CTX')
 target_source_chunks = int(os.environ.get('TARGET_SOURCE_CHUNKS',4))
+PRINT_SOURCE = bool(os.environ.get('PRINT_SOURCE', True))
 
 from constants import CHROMA_SETTINGS
 
@@ -55,9 +56,12 @@ def main():
         print(answer)
 
         # Print the relevant sources used for the answer
-        for document in docs:
-            print("\n> " + document.metadata["source"] + ":")
-            print(document.page_content)
+        if PRINT_SOURCE:
+            for document in docs:
+                print("\n> " + document.metadata["source"] + ":")
+                print(document.page_content)
+        else:
+            print('Printing the source is OFF.')
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description='privateGPT: Ask questions to your documents without an internet connection, '
